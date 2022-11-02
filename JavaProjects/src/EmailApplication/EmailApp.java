4599548e66001db049e03719a04f9eed10acdd2d
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2022-11-01 23:30:57 -0400

    Email Application

---
+++ JavaProjects/src/EmailApplication/EmailApp.java
@@ -0,0 +1,17 @@
+package EmailApplication;
+
+public class EmailApp {
+    public static void main(String[] args) {
+        Email eml = new Email("John", "Smith");
+        eml.changePassword("ADVCNMI254$$%#");
+        String password = eml.getPassword();
+        System.out.println("password = " + password);
+        eml.setAlternativeEmail("abcd@gmail.com");
+        System.out.println("Alternative email is: " +eml.getAlternativeEmail());
+        eml.setMailboxCapacity(4000);
+        System.out.println("Mailbox Capasity increase: " + eml.getMailboxCapacity());
+
+    }
+
+
+}
